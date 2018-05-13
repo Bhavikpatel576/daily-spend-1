34f810f3c02cd5ce3e5b282652c94847e3881339
Sean Smith <sean.smith@paladin.insure>
2018-05-12 18:27:06 -0700

    implemented selenium + plaid manipulator

--- selenium.js
+++ selenium.js
@@ -7,21 +7,37 @@ function sleep(ms) {
 // /div/div/div/div/div/div/input
 
 (async function example() {
-  let driver = await new Builder().forBrowser('firefox').build();
-  // const builder = new Actions(driver);
+  let driver = await new Builder().forBrowser('chrome').build();
+  const Actions = driver.actions;
+  const builder = Actions(driver);
 
   try {
     await driver.get('http://localhost:8000/');
     driver.manage().window().maximize();
     const res = await driver.findElement(By.tagName('button')).sendKeys('webdriver', Key.RETURN);
-    await driver.switchTo().frame(0);
+
+    await driver.switchTo().frame('plaid-link-iframe-1');
     await sleep(1000);
-    const element = driver.findElement(By.xpath("html/body/div/div/div/div/div/div/div/input"));
-    await element.sendKeys("chase");
-    await element.click();
-    // builder.moveToElement(element, X, Y).click().build().perform();
+
+    const input = driver.findElement(By.xpath("html/body/div/div/div/div/div/div/div/input"));
+    await input.sendKeys("chase");
+
+    await sleep(1000);
+
+    const bank = driver.findElement(By.xpath("html/body/div/div/div/div/div/div/div/div/ul/li"));
+    await bank.click();
+
+    await sleep(1000);
+
+    const inputs = await driver.findElements(By.xpath("html/body/div/div/div/div/div/div/form/div/div/input"));
+    await inputs[0].sendKeys("user_good");
+    await inputs[1].sendKeys("pass_good");
+
+    const submit = driver.findElement(By.xpath("html/body/div/div/div/div/div/div/form/button"));
+    await submit.click();
+
   } finally {
-    await sleep(9000);
+    sleep(9000);
     await driver.quit();
   }
 })();
